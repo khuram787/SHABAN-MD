c3ff836bdf20dcfe5b4b1f5c21392c7112b1e289
khuram787 <khuramssultan@gmail.com>
2025-10-18 20:28:44 +0500

    Update configuration values for bot settings

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMklJUXp5VmNINHZsanZQc1FIZis2MUVPOFZESjN3aG5uOE00bGhIOFpsUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSmo5R3hLUmFlcXdUUUVwNjNEUkd3Q1kycjdWT01mdUZxd1lHNDMvMGdGYz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJVTnFtV09tTkNwQXlXT0VhSEJ1dWhWbFprc0gyMks4T05tY1NqNk9RSUVNPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJyeWpRNVhWRzcwcFc1WVRlaThBSDFKUW9VczNBMWFMUE9lUlVMQXhWd2wwPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IndIN1JXYXZoVmtDYWk3QURyZzZtbzFyK3BwbkNlYUVxd0xOSitYeTZ4V2s9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InVzakRXYkQxTXdtY2FZNksvWE81THJJNGRMNDNIbzRxTHFvc2xzeVJjeDQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0Y0dGhrTlBTV2Z5V1pPaTE4UnNVYlVqTTlDVS9Ybm1RWmVqdjQ5NjJVWT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibXh1R1pyZ3ZBSVhqME16b2NUdnFNZFVVZ3ZGRkhzT2lnT2ZLdVJobGxqaz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImhNaWlqR1RObXNaMU5abmwxaUg3UHAwMWNKaUM0WFZkTE1BYTM2TUdPajFxWmRPdjlINUJtdTMyMXllaTJ3WUhzOWhtQmExd2FZdjRTdkN0VWdWd0NBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTU0LCJhZHZTZWNyZXRLZXkiOiJDSUZVVVE4OUhzcjRWcDltVHErU0hySC81ZG51K0NlTGllcTJwU2U0U3ZNPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwicmVnaXN0ZXJlZCI6dHJ1ZSwicGFpcmluZ0NvZGUiOiJQOFpMM0VNQSIsIm1lIjp7ImlkIjoiOTIzMTY2NjIzMDM0OjExQHMud2hhdHNhcHAubmV0IiwibmFtZSI6Im1pbmFoaWwiLCJsaWQiOiIxNzI4MDAwNzM1MzE0MjU6MTFAbGlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNPdlFrWlFFRU8vY3pzY0dHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJtaU5FcFlWeXBlcGx2ZzAwWmw2aXVuNWNhSExObElweFIvR2dXTjB1QWdRPSIsImFjY291bnRTaWduYXR1cmUiOiJxYVlOclNrbGlyeUoxdkx4dCtYMXlSQnF0Q2hzR002bjdFWjZCREgxbHcwa2tQRDRDUFlkZnNoTVZ4Nmx1ZTJqbFRGdVowVzhOK3B0TjByMENkc0pCZz09IiwiZGV2aWNlU2lnbmF0dXJlIjoid1VROVA2Vm92aFdNRGRUWTIxK3hwL2F4cUZpRmpXRG9rc0Q5SjJUOWx1Znl6UVlkNUxQdGJUUCtUZytGVjQ3WDhkdmcwNTh6a1JFN0wxMzh3cHJZQVE9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5MjMxNjY2MjMwMzQ6MTFAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCWm9qUktXRmNxWHFaYjROTkdaZW9ycCtYR2h5elpTS2NVZnhvRmpkTGdJRSJ9fV0sInBsYXRmb3JtIjoic21iYSIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0FVSUFnPT0ifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzYwODAwMzcxLCJsYXN0UHJvcEhhc2giOiIzUjlaMzkifQ==",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -25,7 +25,7 @@ PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
 // add custom menu and mention reply image url   
-BOT_NAME: process.env.BOT_NAME || "SHABAN-MD V5",
+BOT_NAME: process.env.BOT_NAME || "RED SQUARD",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "Mʀ-Sʜᴀʙᴀɴ",
 // type sticker pack name 
@@ -35,9 +35,9 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself  
 OWNER_NUMBER: process.env.OWNER_NUMBER || "923059395959",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "MR-SHABAN",
+OWNER_NAME: process.env.OWNER_NAME || "RED SQUARD",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ Sʜᴀʙᴀɴ*",
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ RED SQUARD*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
 // add img for alive msg 
